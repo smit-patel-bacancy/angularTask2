bd64eeeaf1b8e82f0aa62624f78c32e7e4c80637
sp243024 <psmit972@gmail.com>
2020-12-08 17:21:54 +0530

    addRecord and editRecord are merged

--- src/app/add-records/add-records.component.ts
+++ src/app/add-records/add-records.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { recordType } from '../shared/recordType.model';
 
 @Component({
   selector: 'app-add-records',
@@ -7,11 +9,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./add-records.component.css']
 })
 export class AddRecordsComponent implements OnInit {
-  public firstName: string;
-  public lastName: string;
-  constructor(private http: HttpClient) { }
+  public recordDetails: recordType;
+  // public firstName: string;
+  // public lastName: string;
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.recordDetails = { firstName: "", lastName: "", avatar: "", id: null };
+  }
 
   ngOnInit(): void {
+    console.log(this.route.snapshot.params.id);
+    if (this.route.snapshot.params.id !== "new") {
+      this.recordDetails.id = this.route.snapshot.params.id;
+      this.recordDetails.firstName = "";
+      this.recordDetails.lastName = "";
+    }
   }
 
   public onSubmit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,18 @@ import { AppComponent } from './app.component';
 import { ShowRecordsComponent } from './show-records/show-records.component';
 import { AddRecordsComponent } from './add-records/add-records.component';
 import { RouterModule, Routes } from '@angular/router';
-import { EditRecordsComponent } from './show-records/edit-records/edit-records.component';
 import { FormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
   { path: 'recordlist', component: ShowRecordsComponent },
-  { path: 'recordlist/new', component: AddRecordsComponent },
-  { path: 'recordlist/:id', component: EditRecordsComponent }
+  { path: 'recordlist/:id', component: AddRecordsComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowRecordsComponent,
-    AddRecordsComponent,
-    EditRecordsComponent
+    AddRecordsComponent
   ],
   imports: [
     BrowserModule,
